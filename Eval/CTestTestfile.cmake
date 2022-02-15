4f7000d76160662a050d5e201fa2c5ef4b7946d8
ning.luo <ning.luo@yale.edu>
2022-02-15 13:18:33 -0500

    clean up

--- Eval/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nl437/code/ppsat/Eval
-# Build directory: /home/nl437/code/ppsat/Eval
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nl437/code/ppsat/src
-# Build directory: /home/nl437/code/ppsat/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
