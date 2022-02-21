50af521574749c8140a8f25698deb64a187c910c
NING LUO <32177513+Ning0Luo@users.noreply.github.com>
2022-02-21 13:28:32 -0500

    Update runTests.sh

--- test/runTests.sh
+++ test/runTests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # redirect the first process to null
-./runTests 1 12345 >/dev/null 2>/dev/null &
+test/runTests 1 12345 >/dev/null 2>/dev/null &
 # display the second process
-./runTests 2 12345
+test/runTests 2 12345
