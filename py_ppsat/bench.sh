f4fb30473f73c25851f70f56ae52904fa6791a2c
NING LUO <32177513+Ning0Luo@users.noreply.github.com>
2022-02-21 14:33:03 -0500

    Update bench.sh

--- py_ppsat/bench.sh
+++ py_ppsat/bench.sh
@@ -8,7 +8,7 @@ for genotype in n3r3 n3r4 n3r5 n6r6 n6r8 n6r10; do
         casenum=$(echo $filename | sed 's/genos.haps.\(.*\).cnf/\1/')
         for h in det rand wrand; do
             # echo $n $num $h
-            python3 ppsat.py $fullfilename $genotype $casenum $h
+            python3 ./ppsat.py $fullfilename $genotype $casenum $h
         done
     done
 done
