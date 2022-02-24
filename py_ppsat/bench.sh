dfb7e9859e52845c3b414fbb0bcb24f082ad5532
NING LUO <32177513+Ning0Luo@users.noreply.github.com>
2022-02-24 12:16:26 -0500

    Update bench.sh

--- py_ppsat/bench.sh
+++ py_ppsat/bench.sh
@@ -2,7 +2,7 @@
 
 # NOTE: download data then specify its path in "basedir".
 basedir=genos2sat/benchmarks
-for genotype in n3r3 n3r4 n3r5 n6r6 n6r8 n6r10; do
+for genotype in n3r3; do
     for fullfilename in $basedir/$genotype/*.cnf; do
         filename=$(basename $fullfilename)
         casenum=$(echo $filename | sed 's/genos.haps.\(.*\).cnf/\1/')
